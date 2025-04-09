22f29866d64d62a079444f25afc5d720969729cd
Roman Chaban <roman.chaban1001@gmail.com>
2025-04-09 21:36:29 +0300

    feat: add new pages for Calendar, Employees, InfoPortal, Messenger, and Vacations

---
+++ src/app/info-portal/page.tsx
@@ -0,0 +1,3 @@
+export default function InfoPortal() {
+   return <section>InfoPortal</section>;
+}
---
+++ src/app/messenger/page.tsx
@@ -0,0 +1,3 @@
+export default function Messenger() {
+   return <section>Messenger</section>;
+}
